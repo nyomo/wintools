56020c12e4bb9926e76eb9e0f021caeaf8417c50
nyomoko <nyomoko@gmail.com>
2020-12-09 16:01:29 +0900

    Pathにカレントディレクトリ含めちゃう

--- Autopilot/GetAutopilotDeviceInfo.bat
+++ Autopilot/GetAutopilotDeviceInfo.bat
@@ -1,5 +1,5 @@
 @powershell -NoProfile -InputFormat None -ExecutionPolicy Unrestricted "$s=[scriptblock]::create((gc \"%~f0\"|?{$_.readcount -gt 1})-join\"`n\");&$s" %*&goto:eof
 
-Set-Item Env:Path "$Env:Path;C:\Program Files\WindowsPowerShell\Scripts;D:\"
+Set-Item Env:Path "$Env:Path;C:\Program Files\WindowsPowerShell\Scripts;.\"
 Get-WindowsAutoPilotInfo.ps1  -OutputFile temp.csv
 if (Test-Path .\AutoPilotHWID.csv){ get-content .\temp.csv -last 1 >> AutoPilotHWID.csv} else {get-content .\temp.csv > AutoPilotHWID.csv}
